e0cc3055825846be391a569c639de3f1a6baf6c7
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-10-26 01:43:46 +0200

    Update Options.cpp

--- Projet C++/Options.cpp
+++ Projet C++/Options.cpp
@@ -1,11 +1,11 @@
 #include "Options.h"
 #include <iostream>
 
-//Constructeur qui initialise _expiry
+// Constructeur qui initialise _expiry
 Option::Option(double expiry) : _expiry(expiry) {}
 
-//Getter pour _expiry
-double Option::getExpiry() const
-{
+// Méthode getter pour _expiry
+double Option::getExpiry() const {
     return _expiry;
 }
+
