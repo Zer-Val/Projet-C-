083b95cb85e8b807a18e61b7c48eae9239aad84f
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-10-26 01:40:32 +0200

    BlackScholesPricer.cpp

--- Projet C++/BlackScholesPricer.cpp
+++ Projet C++/BlackScholesPricer.cpp
@@ -1,44 +1,24 @@
-#include "BlackScholesPricer.h"
+#pragma once
 #include <cmath>
-#include <iostream>
+#include "EuropeanVanillaOption.h"
 
-#ifndef M_SQRT1_2 // check if M_SRQT1_2 is not defined
-#define M_SQRT1_2 0.70710678118654752440 // define M_SQRT1_2 as square root of 1/2
-#endif
+#ifndef BLACKSCHOLESPRICER_H
+#define BLACKSCHOLESPRICER_H
 
-//Constructeur de la classe BSP
-BlackScholesPricer::BlackScholesPricer(EuropeanVanillaOption* option, double asset_price, double interest_rate, double volatility)
-    : option_(option), asset_price(asset_price), interest_rate(interest_rate), volatility(volatility) {}
+class BlackScholesPricer {
+public:
+    
+    BlackScholesPricer(EuropeanVanillaOption* option, double asset_price, double interest_rate, double volatility); //Constructeur de la classe BSP
+    double operator()() const; //OpÃ©rateur () pour calculer le prix de l'option
+    double delta() const; //MÃ©thode pour calculer le delta de l'option
 
-//Méthode pour calculer la Frep cumulative de la loi normale
-double BlackScholesPricer::normal_cdf(double x) const {
-    return 0.5 * std::erfc(-x * M_SQRT1_2);
-}
+private: 
+	EuropeanVanillaOption* option_; //Pointeur vers l'option qu'on veut pricer
+	double asset_price_; //Prix de l'actif sous-jacent (S)
+	double interest_rate_; //Taux d'intÃ©rÃªt (r)
+	double volatility_;  //VolatilitÃ© (vol)
 
-//On définit l'opérateur () pour retourner le prix d'une option (Call ou Put)
-double BlackScholesPricer::operator()() const {
-    double K = option_->getStrike();
-    double T = option_->getExpiry();
-    double d1 = (std::log(asset_price / K) + (interest_rate + 0.5 * volatility * volatility) * T) / (volatility * std::sqrt(T));
-    double d2 = d1 - volatility * std::sqrt(T);
+    double normal_cdf(double x) const; //MÃ©thode pour calculer la fonction de rÃ©partition cumulative de la loi normale
+};
 
-    if (option_->GetOptionType() == EuropeanVanillaOption::optionType::call) {
-        return asset_price * normal_cdf(d1) - K * std::exp(-interest_rate * T) * normal_cdf(d2);
-    }
-    else {
-        return K * std::exp(-interest_rate * T) * normal_cdf(-d2) - asset_price * normal_cdf(-d1);
-    }
-}
-
-//Méthode pour retourner le Delta d'une option (Call ou Put)
-double BlackScholesPricer::delta() const {
-    double T = option_->getExpiry();
-    double d1 = (std::log(asset_price / option_->getStrike()) + (interest_rate + 0.5 * volatility * volatility) * T) / (volatility * std::sqrt(T));
-
-    if (option_->GetOptionType() == EuropeanVanillaOption::optionType::call) {
-        return normal_cdf(d1);
-    }
-    else {
-        return normal_cdf(d1) - 1;
-    }
-}
+#endif //BLACKSCHOLESPRICER_H
