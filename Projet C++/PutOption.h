abe1fee1affbec0f86a416531258390915bf4610
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-10-26 01:44:43 +0200

    Update PutOption.h

--- Projet C++/PutOption.h
+++ Projet C++/PutOption.h
@@ -1,18 +1,21 @@
 #pragma once
 #include "EuropeanVanillaOption.h"
+
 #ifndef PUTOPTION_H
 #define PUTOPTION_H
 
-class PutOption : public EuropeanVanillaOption {
+class PutOption : public EuropeanVanillaOption 
+{
     public:
-        // Constructeur qui utilise celui d'EVO
-        PutOption(double expiry, double strike);
+        //Constructeur qui utilise celui d'EVO
+        PutOption(double expiry, double strike): EuropeanVanillaOption(expiry, strike) {}
 
-        // Implémentation de payoff pour une Put
+        //ImplÃ©mentation de payoff pour une Put
         double payoff(double z) const override;
 
-		// Implémentation de GetOptionType pour une Put
+		//ImplÃ©mentation de GetOptionType pour une Put
         optionType GetOptionType() const override;
 };
-#endif // PUTOPTION_H
+
+#endif //PUTOPTION_H
 
