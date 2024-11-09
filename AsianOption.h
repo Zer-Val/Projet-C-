bacd0f3e25091d808e6c2ad2c0ea6df9ed04a5f4
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-11-09 12:28:18 +0100

    Ajout des classes Asian

---
+++ AsianOption.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Option.h"
+#include <vector>
+
+#ifndef ASIANOPTION_H
+#define ASIANOPTION_H
+
+class AsianOption : public Option {
+public:
+    // Constructeur qui initialise _expiry et _timeSteps
+    AsianOption(double expiry, const std::vector<double>& timeSteps);
+
+    // Getter pour _timeSteps
+    const std::vector<double>& getTimeSteps() const;
+
+    // Override de la méthode payoffPath
+    double payoffPath(const std::vector<double>& prices) const override;
+
+    // Override de la méthode isAsianOption
+    bool isAsianOption() const override;
+
+private:
+    std::vector<double> _timeSteps; // Membre privé pour les étapes de temps
+};
+
+#endif // ASIANOPTION_H
