1970617cf0871758b2b2b6d91f54c11ee80ccea8
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-11-11 16:33:19 +0100

    Update AsianOption.h

--- ProjetC++/AsianOption.h
+++ ProjetC++/AsianOption.h
@@ -8,19 +8,19 @@
 class AsianOption : public Option {
 public:
     // Constructeur qui initialise _expiry et _timeSteps
-    AsianOption(double expiry, const std::vector<double>& timeSteps);
+    AsianOption(const std::vector<double>& timeSteps);
 
     // Getter pour _timeSteps
     const std::vector<double>& getTimeSteps() const;
 
-    // Override de la méthode payoffPath
+    // Override de la mÃ©thode payoffPath
     double payoffPath(const std::vector<double>& prices) const override;
 
-    // Override de la méthode isAsianOption
+    // Override de la mÃ©thode isAsianOption
     bool isAsianOption() const override;
 
 private:
-    std::vector<double> _timeSteps; // Membre privé pour les étapes de temps
+    std::vector<double> _timeSteps; // Membre privÃ© pour les Ã©tapes de temps
 };
 
 #endif // ASIANOPTION_H
