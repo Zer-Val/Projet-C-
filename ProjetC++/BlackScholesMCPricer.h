f8fda032510786c206b530eb9ad7927f69f51e84
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-11-30 00:46:49 +0100

    Update BlackScholesMCPricer.h

--- ProjetC++/BlackScholesMCPricer.h
+++ ProjetC++/BlackScholesMCPricer.h
@@ -6,20 +6,29 @@
 #include <cmath>
 #include <stdexcept>
 
-class BlackScholesMCPricer {
-public:
-    BlackScholesMCPricer(Option* option, double initial_price, double interest_rate, double volatility);
+class BlackScholesMCPricer 
+{
+    public:
+		// Constructor of the BlackScholesMCPricer class
+        BlackScholesMCPricer(Option* option, double initial_price, double interest_rate, double volatility);
 
-    void generate(int nb_paths);
-    double operator()() const;
-    std::vector<double> confidenceInterval() const;
-    int getNbPaths() const;
+		// Method to generate paths
+        void generate(int nb_paths);
 
-private:
-    Option* option;
-    double S0;
-    double r;
-    double sigma;
-    int nb_paths_generated; //the number of paths already generated since the beginning of the program
-    double current_estimate;
+		// Operator () to price the option
+        double operator()() const;
+
+		// Method to compute the 95% confidence interval of theprice
+        std::vector<double> confidenceInterval() const;
+
+		// Method to get the number of paths generated sicne the beginning of the program
+        int getNbPaths() const;
+
+    private:
+        Option* option; 
+		double S0; // Underlying asset price
+		double r; // Risk-free interest rate
+		double sigma; // Volatility
+        int nb_paths_generated; //the number of paths already generated since the beginning of the program
+        double current_estimate;
 };
