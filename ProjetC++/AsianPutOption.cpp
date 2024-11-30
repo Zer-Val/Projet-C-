7f91e09b7e75ee5a3a8ad5b45a385bb1bfba03cb
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-11-30 01:00:00 +0100

    Update AsianPutOption.cpp

--- ProjetC++/AsianPutOption.cpp
+++ ProjetC++/AsianPutOption.cpp
@@ -2,10 +2,10 @@
 #include <algorithm> 
 
 // Constructeur qui initialise _timeSteps et _strikePrice
-AsianPutOption::AsianPutOption(const std::vector<double>& timeSteps, double strike)
-    : AsianOption(timeSteps), _strike(strike) {}
+AsianPutOption::AsianPutOption(const std::vector<double>& timeSteps, double strike) : AsianOption(timeSteps), _strike(strike) {}
 
 // Implémentation de la méthode payoff pour l'option Put
-double AsianPutOption::payoff(double underlyingPrice) const {
+double AsianPutOption::payoff(double underlyingPrice) const 
+{
     return std::max(_strike - underlyingPrice, 0.0);
 }
