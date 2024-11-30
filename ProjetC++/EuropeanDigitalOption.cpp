231233bf0210bae42e7477ed7da276f8c6c4f3ea
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-11-30 01:13:40 +0100

    Update EuropeanDigitalOption.cpp

--- ProjetC++/EuropeanDigitalOption.cpp
+++ ProjetC++/EuropeanDigitalOption.cpp
@@ -1,19 +1,22 @@
 #include "EuropeanDigitalOption.h"
 
 // Constructeur qui initialise _expiry et _strike
-EuropeanDigitalOption::EuropeanDigitalOption(double K, double expiry) 
-    : Option(expiry), strike(K) {
-    if (expiry < 0 || strike < 0) {
+EuropeanDigitalOption::EuropeanDigitalOption(double K, double expiry) : Option(expiry), strike(K) 
+{
+    if (expiry < 0 || strike < 0) 
+    {
         throw std::invalid_argument("Expiry and strike must be non-negative");
     }
 }
 
 // Méthode getter pour _strike
-double EuropeanDigitalOption::getStrike() const {
-    return strike;
+double EuropeanDigitalOption::getStrike() const 
+{
+    return _strike;
 }
 
 //Override de isAsianOption pour retourner false pour EVDO - (Probleme de compilation si on ne le fait pas)
-bool EuropeanDigitalOption::isAsianOption() const {
+bool EuropeanDigitalOption::isAsianOption() const 
+{
 	return false;
 }
