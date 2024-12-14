e27e57e984e1d0a3a959712ab67d0a7cf6f2bc5a
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-14 17:04:01 +0100

    Update Option.cpp

--- ProjetC++/Option.cpp
+++ ProjetC++/Option.cpp
@@ -1,13 +1,13 @@
 #include "Option.h"
 #include <iostream>
 
-// Implementation of the constructor that initialize _expiry
+// Constructor of Option, that initialize _expiry, in years
 Option::Option(double expiry) : _expiry(expiry) {}
 
-// Implementation of the getter method for _expiry
+// Getter method for _expiry
 double Option::getExpiry() const 
 {
-    return _expiry;
+	return _expiry;
 }
 
 // Implementation of the method payoffPath that calls the method payoff(double) with the last element of the vector
@@ -19,7 +19,7 @@ double Option::payoffPath(const std::vector<double>& prices) const
 // Implementation of the virtual method to check if the option is an Asian Option
 bool Option::isAsianOption() const 
 {
-    return false;
+    	return false;
 }
 
 // Implementation of the virtual method to check if the option is an American Option
