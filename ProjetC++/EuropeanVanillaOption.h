01dcfb2e120f00e381a15af727c43e359892006a
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-14 17:18:41 +0100

    Update EuropeanVanillaOption.h

--- ProjetC++/EuropeanVanillaOption.h
+++ ProjetC++/EuropeanVanillaOption.h
@@ -7,29 +7,23 @@ class BlackScholesPricer; // Forward declaration of the BlackScholesPricer class
 
 class EuropeanVanillaOption : public Option 
 {
-    public:
-        // Enum for the option type
-        enum class optionType { call, put };
+    	public:
+	        // Enum for the option type
+	        enum class optionType { call, put };
+	
+	        //Constructor of EuropeanVanillaOptions, that initialize _strike as a double (K)
+	        EuropeanVanillaOption(double expiry, double strike);
+	
+	        // Getter method for _strike
+	        double getStrike() const;
+	
+	        // Pure virtual method to get the option type
+	        virtual optionType GetOptionType() const = 0;
+	
+	        // BlackScholesPricer class declared as a friend class of EVO to be able to access _strike in BSP
+	        friend class BlackScholesPricer;
 
-        //Constructor that initialize _strike
-        EuropeanVanillaOption(double expiry, double strike);
-
-        // Getter method for _strike
-        double getStrike() const;
-
-        // Pure virtual method to get the option type
-        virtual optionType GetOptionType() const = 0;
-
-        // I declare BSP as a friend class of EVO to be able to access _strike in BSP
-        friend class BlackScholesPricer;
-
-	    // Override of isAsianOption to return false for EVO - (Compilation issue if not)
-        bool isAsianOption() const override;
-
-		// Override of isAmericanOption to return false for EVO
-		bool isAmericanOption() const override;
-
-    private:
-        double _strike; // Private member variable for the strike price
+    	private:
+        	double _strike; // Private member variable for the strike price as a double (K)
 };
 
