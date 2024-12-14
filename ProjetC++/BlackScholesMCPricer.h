e8cb1b51eba4ad94adbb439f30901830aa2e4d3b
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-14 12:18:36 +0100

    Update BlackScholesMCPricer.h
    Updated to deal with Asian options

--- ProjetC++/BlackScholesMCPricer.h
+++ ProjetC++/BlackScholesMCPricer.h
@@ -1,6 +1,6 @@
 #pragma once
-
 #include "Option.h"
+#include "AsianOption.h"
 #include "MT.h"
 #include <vector>
 #include <cmath>
@@ -12,6 +12,9 @@ class BlackScholesMCPricer
 		// Constructor of the BlackScholesMCPricer class
         BlackScholesMCPricer(Option* option, double initial_price, double interest_rate, double volatility);
 
+		// Method to get the number of paths generated sicne the beginning of the program
+		int getNbPaths() const;
+
 		// Method to generate paths
         void generate(int nb_paths);
 
@@ -21,14 +24,18 @@ class BlackScholesMCPricer
 		// Method to compute the 95% confidence interval of theprice
         std::vector<double> confidenceInterval() const;
 
-		// Method to get the number of paths generated sicne the beginning of the program
-        int getNbPaths() const;
+
 
     private:
-        Option* option; 
-		double S0; // Underlying asset price
-		double r; // Risk-free interest rate
-		double sigma; // Volatility
-        int nb_paths_generated; //the number of paths already generated since the beginning of the program
-        double current_estimate;
+        Option* _option; 
+		double _underlying_price; // Underlying asset price (S0)
+		double _risk_free_rate; // Risk-free interest rate (r)
+		double _volatility; // Volatility (sigma)
+        int _nb_paths_generated; // Number of paths already generated since the beginning of the program (Nb)
+		double _current_estimate; // Current estimate of the option price
+
+		double estimates_mean; //
+        double estimates_variance; //
+
+        std::vector<double> estimated_prices; // Store estimated option prices
 };
