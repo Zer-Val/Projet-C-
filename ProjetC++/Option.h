acc27cf1ee1f6644e2c5f05b61cbe0c65a9b8ef1
ZerVal <156453793+Zer-Val@users.noreply.github.com>
2024-12-14 16:59:00 +0100

    Update Option.h

--- ProjetC++/Option.h
+++ ProjetC++/Option.h
@@ -3,26 +3,27 @@
 
 class Option 
 {
-    public:
-        // Constructor that initialize _expiry
-        Option(double expiry);
+	public:
+        
+        	// Constructor of Option, that initialize _expiry, in years
+        	Option(double expiry);
 
-	    // Getter method for _expiry
-        double getExpiry() const;
+	    	// Getter method for _expiry
+        	double getExpiry() const;
 
-	    // Virtual method to calculate the payoff of the option
-        virtual double payoff(double underlyingPrice) const = 0;
+	   	// Virtual method to calculate the payoff of the option
+        	virtual double payoff(double underlyingPrice) const = 0;
 
-	    // Virtual method to calculate the payoff of the option with a path of prices (Asian Options)
-        virtual double payoffPath(const std::vector<double>& prices) const;
+	    	// Virtual method to calculate the payoff of the option with a path of prices (used for Asian Options)
+        	virtual double payoffPath(const std::vector<double>& prices) const;
 
-	    // Virtual method to check if the option is an Asian Option
-        virtual bool isAsianOption() const;
+	    	// Virtual method to check if the option is an Asian Option
+        	virtual bool isAsianOption() const;
 
-	    // Virtual method to check if the option is an American Option
-        virtual bool isAmericanOption() const;
+	    	// Virtual method to check if the option is an American Option
+        	virtual bool isAmericanOption() const;
 
-    private:
-	    double _expiry; //Private attribute that stores the expiry of the option
+    	private:
+	    	double _expiry; //Private attribute that stores the expiry date of the option in years (T)
 };
 
